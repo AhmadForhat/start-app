5026810663b8a1cfc56f85fa3046203c879a8bf4
mingweisui <williesui123@gmail.com>
2023-05-10 10:39:42 +0800

    First Update with Ahmad

--- src/Views/WalkingThrough/WalkingThrough.tsx
+++ src/Views/WalkingThrough/WalkingThrough.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import img from '../../filesrc/images/example.png';
 import {
 	Container,
 	ContainerDescription,
+	ContainerIcon,
+	ContainerSteps,
+	Square,
+	ContainerPageInfo,
 	Description,
 	Title,
-	Image,
 } from './WalkingThrough.styles';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'designSystem';
+import { CloseIcon, Html } from 'designSystem/icons';
 
 export const WalkingThrough = () => {
 	const [currentStep, setCurrentStep] = useState('getCertification');
@@ -19,7 +22,7 @@ export const WalkingThrough = () => {
 
 	const stepProps = {
 		getCertification: {
-			image: img,
+			id: 1,
 			title: 'GET YOUR FREE CERTIFICATION',
 			description:
 				'Do a knowledge practice test in your field and become an expert',
@@ -27,6 +30,7 @@ export const WalkingThrough = () => {
 			onNext: () => setCurrentStep('aboutTheTest'),
 		},
 		aboutTheTest: {
+			id: 2,
 			title: 'ABOUT THE TEST',
 			description:
 				'You will respond 30 multiple choice questions. There is only one correct answer per question',
@@ -34,6 +38,7 @@ export const WalkingThrough = () => {
 			onNext: () => setCurrentStep('timer'),
 		},
 		timer: {
+			id: 3,
 			title: 'SET A TIMER',
 			description:
 				'Make sure to spend your time wisely! You will have 40 mins for completing the test',
@@ -44,8 +49,18 @@ export const WalkingThrough = () => {
 
 	return (
 		<Container data-testid="walking-through">
+			<ContainerIcon>
+				<CloseIcon className="close-icon" />
+			</ContainerIcon>
+			<ContainerPageInfo>
+				<Html />
+				<ContainerSteps>
+					<Square actived={stepProps?.id === 1} />
+					<Square actived={stepProps?.id === 2} />
+					<Square actived={stepProps?.id === 3} />
+				</ContainerSteps>
+			</ContainerPageInfo>
 			<ContainerDescription>
-				{stepProps?.image && <Image src={stepProps?.image} alt="Your Image" />}
 				<Title>{stepProps?.title}</Title>
 				<Description>{stepProps?.description}</Description>
 			</ContainerDescription>
