56ecd553707efdbf7791e052e607eb836ec142b9
ahmad.forhat <ahmad.forhat@welbecare.com>
2023-05-18 19:17:39 -0700

    refactor: :recycle: add new page structure

--- src/Router/utils/index.ts
+++ src/Router/utils/index.ts
@@ -2,6 +2,7 @@ import { Results } from 'Views/Results';
 import { Questions } from 'Views/Questions';
 import { WalkingThrough } from 'Views/WalkingThrough';
 import { SelectFields } from 'Views/SelectFields';
+import { SelectLevel } from 'Views/SelectLevel';
 
 export const publicPages = [
 	{
@@ -15,6 +16,12 @@ export const publicPages = [
 		name: 'Select Fields',
 		path: '/select-fields',
 		element: SelectFields,
+	},
+	{
+		id: 'select-level',
+		name: 'Select Level',
+		path: '/select-level',
+		element: SelectLevel,
 	},
 	{
 		id: 'questions',
---
+++ src/Views/SelectLevel/SelectLevel.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SelectLevel = () => {
+	return <div>SelectLevel</div>;
+};
