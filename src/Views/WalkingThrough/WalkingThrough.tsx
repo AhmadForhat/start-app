5a58be7883414aa4e7968e28b7dde1bcfdf0af1d
ahmad.forhat <ahmad.forhat@welbecare.com>
2023-10-25 20:50:09 -0700

    finish start app walking

--- src/Views/WalkingThrough/WalkingThrough.tsx
+++ src/Views/WalkingThrough/WalkingThrough.tsx
@@ -12,7 +12,10 @@ import {
 } from './WalkingThrough.styles';
 import { Button, Wrapper } from 'designSystem';
 import { useNavigate } from 'react-router-dom';
-import { CloseIcon, Html } from 'designSystem/icons';
+import { CloseIcon } from 'designSystem/icons';
+import { ReactComponent as Diploma } from 'designSystem/icons/diploma.svg';
+import { ReactComponent as DocTest } from 'designSystem/icons/doc-test.svg';
+import { ReactComponent as TimeTracking } from 'designSystem/icons/time-tracking.svg';
 
 export const WalkingThrough = () => {
 	const [currentStep, setCurrentStep] = useState('getCertification');
@@ -29,6 +32,7 @@ export const WalkingThrough = () => {
 				'Do a knowledge practice test in your field and become an expert',
 			buttonText: 'CONTINUE',
 			onNext: () => setCurrentStep('aboutTheTest'),
+			icon: <Diploma />,
 		},
 		aboutTheTest: {
 			id: 2,
@@ -37,6 +41,7 @@ export const WalkingThrough = () => {
 				'You will respond 30 multiple choice questions. There is only one correct answer per question',
 			buttonText: 'CONTINUE',
 			onNext: () => setCurrentStep('timer'),
+			icon: <DocTest />,
 		},
 		timer: {
 			id: 3,
@@ -45,6 +50,7 @@ export const WalkingThrough = () => {
 				'Make sure to spend your time wisely! You will have 40 mins for completing the test',
 			buttonText: 'GET STARTED',
 			onNext: goToSelectFields,
+			icon: <TimeTracking />,
 		},
 	}[currentStep];
 
@@ -55,7 +61,7 @@ export const WalkingThrough = () => {
 					<CloseIcon className="close-icon" />
 				</ContainerIcon>
 				<ContainerPageInfo>
-					<Html />
+					{stepProps?.icon}
 					<ContainerSteps>
 						<Square actived={stepProps?.id === 1} />
 						<Square actived={stepProps?.id === 2} />
